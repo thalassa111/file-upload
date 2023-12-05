0a08370fcd7469aff40a9c6a3bc8e26d2f579b02
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-12-05 16:03:04 +0100

    can now download a file from database

--- src/main/java/com/me/chengspringboot/controllers/FileController.java
+++ src/main/java/com/me/chengspringboot/controllers/FileController.java
@@ -2,9 +2,12 @@ package com.me.chengspringboot.controllers;
 
 import com.me.chengspringboot.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @RestController
 public class FileController {
@@ -23,10 +26,18 @@ public class FileController {
         return fileService.uploadFile(token, folderId, file);
     }
 
-    @DeleteMapping("file/delete-file/{folderId}/{fileName}")
+    @DeleteMapping("/file/delete-file/{folderId}/{fileName}")
     public ResponseEntity<String> deleteFile(   @RequestHeader("Authorization") String token,
                                                 @PathVariable int folderId,
                                                 @PathVariable String fileName){
         return fileService.deleteFile(token, folderId, fileName);
     }
+
+    @GetMapping("/file/download-file/{folderId}/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@RequestHeader("Authorization") String token,
+                                                              @PathVariable int folderId,
+                                                              @PathVariable String fileName){
+        return fileService.downloadFile(token, folderId, fileName);
+    }
+
 }
